ad8f54bf8be420855eb749d095eb61bfdb611c47
John Gray <john.edwin.gray@gmail.com>
2024-09-04 14:58:01 +1200

    fix merge conflicts

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -22,40 +22,6 @@ function App() {
 }
 
 export default App
-import { Routes, Route } from 'react-router-dom'
-import BookSortAndFilter from './BookSortAndFilter'
-import Home from './Home'
-import About from './About'
 
-function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books" element={<BookSortAndFilter />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  )
-}
 
-export default AppRoutes
-function Home() {
-  return (
-    <div>
-      <h2>Welcome to My Books</h2>
-      <p>This is a simple application to manage and view your book collection.</p>
-    </div>
-  )
-}
 
-export default Home
-function About() {
-  return (
-    <div>
-      <h2>About My Books</h2>
-      <p>My Books is a web application designed to help book enthusiasts manage their personal library.</p>
-      <p>Created with React, Express, and SQLite.</p>
-    </div>
-  )
-}
-
-export default About
