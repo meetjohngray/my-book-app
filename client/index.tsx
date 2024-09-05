2195a4605fd83bb06661feb3ba748001c324b413
John Gray <john.edwin.gray@gmail.com>
2024-09-05 14:52:05 +1200

    set-up for deployment

--- client/index.tsx
+++ client/index.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import App from './components/App.tsx'
+import App from './components/App'
 
 const queryClient = new QueryClient()
 
